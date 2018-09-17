84df4b5392b3b55970bfe30cfcfd452171e60734
Patrik <patrik@assured.se>
2018-09-17 11:38:36 +0200

    Added application and Dockerfile

---
+++ webapp.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return get_content()
+
+def get_content():
+    # Implemement DB support later
+    return "HELLO THE WORLD!"
+
+if __name__ == "__main__":
+    app.run(host='localhost',port=5000)
