6055813975fd0ac085dc85339a22baafb853e74b
Patrik <patrik@assured.se>
2018-09-17 16:52:11 +0200

    Bugfix2

--- webapp.py
+++ webapp.py
@@ -45,9 +45,9 @@ def init_db():
         db_connection.commit()
 
     except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        elif err.errno == mysql.connectorerrorcode.ER_BAD_DB_ERROR:
             print("Database does not exist")
         else:
             print(err)
